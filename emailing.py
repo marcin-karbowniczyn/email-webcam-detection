0e0202af98e320827c30705dd6ab484b57a0e799
Marcin <marcin.karbowniczyn@gmail.com>
2023-12-18 12:36:59 +0100

    Initial commit, detect new objects in the camera, send a frame with detected object via email

---
+++ emailing.py
@@ -0,0 +1,38 @@
+import smtplib
+import imghdr  # This library gives us metadata about images
+from os import getenv
+from dotenv import load_dotenv
+from email.message import EmailMessage
+
+load_dotenv()
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message['Subject'] = 'New camera detection'
+    # Email body
+    email_message.set_content('Hey, someone has beed detected by the camera.')
+
+    # Read binary, for the image
+    with open(image_path, 'rb') as file:
+        content = file.read()
+    # mghdr.what() this method will fine out what kind of image is in the content
+    email_message.add_attachment(content, maintype='image', subtype=imghdr.what(None, content))
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.ehlo()
+        server.starttls()
+        server.login(getenv('GMAIL_SENDER'), getenv('GMAIL_PASSWORD'))
+        server.sendmail(getenv('GMAIL_SENDER'), getenv('EMAIL_RECIEVER'), email_message.as_string())
+
+    # THE WAY WE WOULD DO IT WITOUT WITH CONTEXT MANAGER
+    # gmail = smtplib.SMTP('smtp.gmail.com', 587)
+    # gmail.ehlo()
+    # gmail.starttls()
+    # gmail.login(sender, password)
+    # gmail.sendmail(sender, reciever, email_message.as_string())
+    # gmail.quit()
+
+
+if __name__ == '__main__':
+    send_email(image_path='images/19.png')
---
+++ main.py
@@ -0,0 +1,68 @@
+import cv2
+import time
+import glob
+from emailing import send_email
+
+video = cv2.VideoCapture(0)
+
+# Stops the execution of the code for 1 second for the camera to start
+time.sleep(1)
+
+first_frame = None
+status_list = []
+count = 1
+
+while True:
+    status = 0
+    check, frame = video.read()
+
+    # 1. We change the pixels of the frame to gray color.
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # 2. Then we use blur on it. We do it to make the frames less precise, to be easier and faster to compare with each other.
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    # 3. We compare every frame with the original frame. Delta frame means pixels, which aren't in the first frame. So this is a difference between the first frame, and a current frame.
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    # 4. We want to make whiteish pixels more white, more explicit. We want the objects to be more white. This method changes every pixel which is 30 or more into 255.
+    # cv2.THRESH_BINARY -> algorithm we use for the conversion
+    thresh_frame = cv2.threshold(delta_frame, 90, 255, cv2.THRESH_BINARY)[1]
+
+    # 5. We try to remove the "noise" from the video. The higher iterations, the more proccessing will be applied to this method.
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=3)
+
+    # 6. Detect the contours around white areas. Countours is a list of all detected countours. cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE -> algorithms
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        # If the contour is less than 5000 pixels, it is a light noise, not an object.
+        if cv2.contourArea(contour) < 3000:
+            continue
+        # If the counter is more than 3000 pixels, we draw a rectangle. 24:50 na filmie 307
+        x, y, w, h = cv2.boundingRect(contour)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, x + y), (0, 0, 255), 3)
+        if rectangle.any():
+            status = 1
+            # If there is a rectangle around the object, save this frame
+            if count < 30:
+                cv2.imwrite(f"images/{count}.png", frame)
+                count += 1
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+    if status_list[0] and not status_list[1]:  # if status_list[0] == 1 and status_list[1] == 0
+        # Choose which frame will be sent via email
+        all_images = glob.glob('images/*.png')
+        image_to_send = all_images[int(len(all_images) / 2)]
+        send_email(image_to_send)
+
+    # 7. Display the video with rectangles
+    cv2.imshow('My Video', frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+video.release()
